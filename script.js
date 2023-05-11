85bcbf3063954882a4f8224d39f57a5e80e9eee5
jaydww <jamesdwalsh@hotmail.com>
2023-05-11 09:45:36 -0700

    pull items from local storage and create init function

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 
+function displayItems(){
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach(item => addItemtoDOM(item));
+
+  checkUI();
+}
+
 function onAddItemSubmit(e){
   e.preventDefault();
   
@@ -39,22 +46,6 @@ function addItemtoDOM(item){
   itemList.appendChild(li);
 }
 
-function addItemToStorage(item){
-  let itemsFromStorage;
-
-  if(localStorage.getItem('items') === null){
-    itemsFromStorage = [];
-  } else {
-    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
-  }
-
-  //Add new item to array
-  itemsFromStorage.push(item);
-
-  //Convert to JSON string and set to local storage
-  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
-}
-
 function createButton(classes){
   const button = document.createElement('button');
   button.className = classes;
@@ -67,6 +58,34 @@ function createIcon(classes){
   const icon = document.createElement('i');
   icon.className = classes;
   return icon;
+}
+
+function addItemToStorage(item){
+  let itemsFromStorage;
+
+  if(localStorage.getItem('items') === null){
+    const itemsFromStorage = getItemsFromStorage();
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  //Add new item to array
+  itemsFromStorage.push(item);
+
+  //Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage(){
+  let itemsFromStorage;
+
+  if(localStorage.getItem('items') === null){
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  return itemsFromStorage;
 }
 
 function removeItem(e){
@@ -112,11 +131,18 @@ function checkUI(){
   }
 }
 
-//Event listeners
-itemForm.addEventListener('submit', onAddItemSubmit);
-itemList.addEventListener('click', removeItem);
-itemList.addEventListener('click', removeItem);
-clearButton.addEventListener('click', clearItems);
-itemFilter.addEventListener('input', filterItems);
+//Initialize app
+function init(){
+  //Event listeners
+  itemForm.addEventListener('submit', onAddItemSubmit);
+  itemList.addEventListener('click', removeItem);
+  itemList.addEventListener('click', removeItem);
+  clearButton.addEventListener('click', clearItems);
+  itemFilter.addEventListener('input', filterItems);
+  document.addEventListener('DOMContentLoaded', displayItems)
+  
+  checkUI();
+}
 
-checkUI();
+init();
+
